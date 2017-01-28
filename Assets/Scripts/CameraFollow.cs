0bd59a5c124ed44e4e14bb1b6146d3b37c8acc88
castro-vini85 <castro.vini85@gmail.com>
2017-01-28 09:08:20 -0800

    Project Creation
    First commit after project creation

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject target;
+    public float xOffset = 0;
+    public float yOffset = 0;
+    public float zOffset = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void LateUpdate()
+    {
+        if (target.transform.position.y >= (yOffset * -1))
+        {
+            this.transform.position = new Vector3(target.transform.position.x + xOffset,
+                                                  target.transform.position.y + yOffset,
+                                                  target.transform.position.z + zOffset);
+        }
+    }
+}
---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+
+    public int initialFuel = 1000;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        if (initialFuel > 0)
+        {
+
+            int nbTouches = Input.touchCount;
+
+            bool jetpackActive = nbTouches > 0;
+
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10));
+
+            if (jetpackActive)
+            {
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 15));
+            }
+
+            initialFuel--;
+        }
+
+        if (initialFuel == 0)
+        {
+            var ps = GetComponentInChildren<ParticleSystem>().emission;
+            ps.enabled = false;
+        }
+    }
+}
